29081f1927417b132f0685f54302d3b0173f7331
nomrk <nomrk@localhost.localdomain>
2021-08-30 20:11:57 +0200

    Chapter 5 done.

---
+++ src/main/java/com/home/Dollar.java
@@ -0,0 +1,19 @@
+package com.home;
+
+public class Dollar {
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiply) {
+        return new Dollar(amount * multiply);
+    }
+
+    @Override
+    public boolean equals(Object object){
+        Dollar dollar = (Dollar)object;
+        return amount == dollar.amount;
+    }
+}
--- src/test/java/com/home/MoneyTest.java
+++ src/test/java/com/home/MoneyTest.java
@@ -2,11 +2,20 @@ package com.home;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
-    void testSampleTest(){
-        assertTrue(true == true);
+    void testMultiplication(){
+        Dollar five = new Dollar(5);
+        Dollar produce = five.times(2);
+        assertEquals(10, produce.amount);
+        produce = five.times(3);
+        assertEquals(15, produce.amount);
+    }
+    @Test
+    void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
